477336a9a55ef60eb335279f8f0c2648f43d4ed2
huangqianmin <huangqianmin@126.com>
2024-07-28 18:05:35 +0800

    胜利界面

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,7 +1,18 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
+
 @onready var timer: Timer = $Timer
 
 
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
+	
+
 func get_time() -> int:
 	return int(timer.time_left)
+
+
+func _on_timer_timeout():
+	var victory_instance = victory_screen_scene.instantiate()
+	add_child(victory_instance)
